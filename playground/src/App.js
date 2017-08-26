d1e2f97d1fa7749945975ac26de628c2a2bdebaa
entronad <entronad@gmail.com>
2017-08-26 23:40:43 +0800

    play nav

--- playground/src/App.js
+++ playground/src/App.js
@@ -2,22 +2,90 @@ import React from 'react';
 import {
   AppRegistry,
   Text,
+  View,
+  Button,
 } from 'react-native';
-import {StackNavigator} from 'react-navigation';
+import {
+  StackNavigator,
+  TabNavigator,
+} from 'react-navigation';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
   };
   render() {
+    const {navigate} = this.props.navigation;
     return (
-      <Text>Hello, Navigation!</Text>
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat', {user: 'Tom'})}
+          title="Chat with Lucy"
+        />
+      </View>
     );
   }
 }
 
+class ChatScreen extends React.Component {
+  static navigationOptions = ({navigation}) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
+  render() {
+    const {params} = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user}</Text>
+      </View>
+    );
+  }
+}
+
+
+
+///////////////////////////////////////////////
+
+class RecentChatsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>List of recent chats</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Chat', {user: 'Rec'})}
+          title="Chat with Rec"
+        />
+      </View>
+    );
+  }
+}
+
+class AllContactsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>List of all contact</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Chat', {user: 'All'})}
+          title="Chat with All"
+        />
+      </View>
+    );
+  }
+}
+
+const MainScreenNavigator = TabNavigator({
+  Recent: {screen: RecentChatsScreen},
+  All: {screen: AllContactsScreen},
+});
+
+MainScreenNavigator.navigationOptions = {
+  title: 'My Chats',
+};
+
 const SimpleApp = StackNavigator({
-  Home: {screen: HomeScreen},
+  Home: {screen: MainScreenNavigator},
+  Chat: {screen: ChatScreen},
 });
 
 AppRegistry.registerComponent('playground', () => SimpleApp);
