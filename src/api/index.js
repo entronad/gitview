c8f5f8b48fdb5cb3fc9de9e8289a52fa7cf2d805
entronad <entronad@gmail.com>
2017-11-07 20:29:18 +0800

    log

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,56 @@
-import { client } from '../../secret'
+import { Buffer } from 'buffer';
+import { client } from '../../secret';
+
+const loginUrl = `https://api.github.com/authorizations/clients/${client.id}`;
+const baseUrl = 'https://api.github.com/graphql';
+
+const getBasicAuth = ({
+  username,
+  password,
+}) => {
+  const authBuffer = new Buffer(
+    `${username}:${password}`,
+    'base64'
+  );
+  return `Basic ${authBuffer.toString()}`;
+};
+
+const 
 
 export const api = {
-  
+  login: async ({
+    username,
+    password,
+  }) => {
+    const payload = JSON.stringify({
+      client_secret: client.secret,
+      scopes: [
+      'user',
+      'repo',
+      'delete_repo',
+      'notifications',
+      'gist',
+      'admin:repo_hook',
+      'admin:org_hook',
+      'admin:org',
+      'admin:public_key',
+      'admin:gpg_key',
+      ],
+      note: `${username} on Gitview`,
+    });
+    return fetch(
+      loginUrl,
+      {
+        'PUT',
+        headers: {
+          Authorization: getBasicAuth({
+            username,
+            password,
+          }),
+        },
+        body: payload,
+      }
+    );
+  }
+
 }
