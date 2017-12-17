9f0333dc44b0bbd4680b704f3f8031fa0ae52cee
entronad <entronad@gmail.com>
2017-12-17 22:58:04 +0800

    test store

--- src/components/auth/Login/index.js
+++ src/components/auth/Login/index.js
@@ -3,11 +3,25 @@ import {
   View,
   Text
 } from 'react-native'
+import { connect } from 'react-redux';
 
-export default class Login extends React.Component {
+const mapStateToProps = state => ({
+  authorized: state.auth.authorized,
+})
+
+class Login extends React.Component {
   render() {
+    this.props.dispatch({
+      type: 'LOGIN_SUCCESS',
+      payload: {
+        accessToken: 'response.body.token',
+      }
+    });
+    console.log('** login');
     return (
       <Text>Login</Text>
     );
   }
 }
+
+export default connect(mapStateToProps)(Login);
